f7cd2b6d19fc526ae4973912bb694cb9be22fcf6
rohitrajput1997 <rohitrajput1997>
2022-07-06 10:24:54 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'first_project';
+  ngOnInit() {
+    alert("");
+  }
+}
